29d76169bf2112df53aacc8268afa0469b5c6a9c
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-05-08 22:24:44 -0300

    first commit

--- server.js
+++ server.js
@@ -9,11 +9,22 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
+
+/* ***********************
+ * View Engines and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
