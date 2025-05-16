f912ef77321e08c9454e7cf188a5c9de109f7ea2
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-05-16 19:22:54 -0300

    assignment2 done

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -26,4 +26,3 @@ WHERE c.classification_name = 'Sport';
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
-
