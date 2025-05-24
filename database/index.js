3aa098cc537915577be2d72e459e0fff4bfd7272
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-05-24 00:29:03 -0300

    fixed setup

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 let pool
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
